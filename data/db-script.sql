a9251323fd9cf2cdc47ccd08c0502f23c07c2d04
Quốc Bình <105304905+BinhJim@users.noreply.github.com>
2022-05-26 08:56:29 +0700

    Update db-script.sql

--- data/db-script.sql
+++ data/db-script.sql
@@ -23,18 +23,19 @@ CREATE TABLE tblUser
 	fullName varchar(50) NOT NULL,
 	phone varchar(20),
 	email varchar(50),
-	roleID varchar(50)
+	roleID varchar(50),
+	status tinyint
 );
 GO
 
 INSERT INTO tblUser(username,password,fullName,email,phone,roleID) 
-VALUES(N'customer1', N'1', N'Nguyen Van A', N'abc1@gmail.com', N'0780780789', N'US'),
-      (N'customer2', N'1', N'Nguyen Van B', N'abc2@gmail.com', N'0780780790', N'US'),
-	  (N'admin', N'1', N'Tao la Admin', N'admin@gmail.com', N'0780780791', N'AD'),
-      (N'dev1', N'1', N'dev1', N'hungpdse150222@fpt.edu.vn', N'0780780792', N'US'),
-      (N'dev2', N'1', N'dev2', N'baohngse150260@fpt.edu.vn', N'0780780793', N'US'),
-      (N'dev3', N'1', N'dev3', N'duylhse150233@fpt.edu.vn', N'0780780794', N'US'),
-	  (N'dev4', N'1', N'dev4', N'binhlqse150199@fpt.edu.vn', N'0780780795', N'US')
+VALUES(N'customer1', N'1', N'Nguyen Van A', N'abc1@gmail.com', N'0780780789', N'US',1),
+      (N'customer2', N'1', N'Nguyen Van B', N'abc2@gmail.com', N'0780780790', N'US',1),
+	  (N'admin', N'1', N'Tao la Admin', N'admin@gmail.com', N'0780780791', N'AD',1),
+      (N'dev1', N'1', N'dev1', N'hungpdse150222@fpt.edu.vn', N'0780780792', N'US',1),
+      (N'dev2', N'1', N'dev2', N'baohngse150260@fpt.edu.vn', N'0780780793', N'US',1),
+      (N'dev3', N'1', N'dev3', N'duylhse150233@fpt.edu.vn', N'0780780794', N'US',1),
+	  (N'dev4', N'1', N'dev4', N'binhlqse150199@fpt.edu.vn', N'0780780795', N'US',1)
 GO
 
 IF OBJECT_ID('dbo.tblRole', 'u') IS NOT NULL 
