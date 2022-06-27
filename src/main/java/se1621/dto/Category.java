ddac39b85be10b50fff1007189ef09fbe888734a
Hungpd170501 <hungpd170501@gmail.com>
2022-06-27 20:51:26 +0700

    Category.java

--- src/main/java/se1621/dto/Category.java
+++ src/main/java/se1621/dto/Category.java
@@ -1,12 +1,8 @@
 package se1621.dto;
 
+import lombok.*;
+
 import java.sql.Date;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
@@ -21,5 +17,5 @@ public class Category {
     public Date createdDate;
     public Date lastModifiedDate;
     public int categoryStatus;
-    
+
 }
