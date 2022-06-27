417cb870bc5fde10befa157067de1e22d44fad53
Hungpd170501 <hungpd170501@gmail.com>
2022-06-27 21:22:47 +0700

    ResumeSkill.java

--- src/main/java/se1621/dto/ResumeSkill.java
+++ src/main/java/se1621/dto/ResumeSkill.java
@@ -4,13 +4,7 @@
  */
 package se1621.dto;
 
-import java.util.Map;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 /**
  *
