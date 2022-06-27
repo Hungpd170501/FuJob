9e026c7f6f9ecab3da1811380648ea816a032952
Hungpd170501 <hungpd170501@gmail.com>
2022-06-27 21:22:02 +0700

    Resume.java

--- src/main/java/se1621/dto/Resume.java
+++ src/main/java/se1621/dto/Resume.java
@@ -1,12 +1,8 @@
 package se1621.dto;
 
+import lombok.*;
+
 import java.sql.Date;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
