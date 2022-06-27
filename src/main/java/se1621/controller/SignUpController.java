81ed894dcd1b774d901c0daf40cc1c9baac2904c
Hungpd170501 <hungpd170501@gmail.com>
2022-06-27 21:27:06 +0700

    SignUpController.java

--- src/main/java/se1621/controller/SignUpController.java
+++ src/main/java/se1621/controller/SignUpController.java
@@ -4,21 +4,21 @@
  */
 package se1621.controller;
 
-import se1621.dto.Error.UserError;
-import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import se1621.dao.UserDAO;
+import se1621.dto.Error.UserError;
 import se1621.dto.Role;
 import se1621.dto.User;
 import se1621.service.EmailServiceImpl;
 import se1621.utils.Helper;
 
+import java.io.IOException;
+
 /**
- *
  * @author HNGB
  */
 @WebServlet(name = "SignUpController", urlPatterns = {"/SignUpController"})
