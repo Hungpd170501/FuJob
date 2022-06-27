d7222aee8e29071a70242b5d1c6785ef62e892c0
Hungpd170501 <hungpd170501@gmail.com>
2022-06-27 21:05:07 +0700

    DBUtils.java Checks failed: 4 warnings

--- src/main/java/se1621/utils/DBUtils.java
+++ src/main/java/se1621/utils/DBUtils.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package se1621.utils;
 
 import java.sql.Connection;
@@ -9,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- *
  * @author ACER
  */
 public class DBUtils {
@@ -17,21 +12,22 @@ public class DBUtils {
     private static DBUtils instance;
     private Connection connection;
 
-    private final String serverName = "localhost";
-    private final String dbName = "FuJobDB_1";
+    private final String serverName = "fujob.database.windows.net";
+    private final String dbName = "FuJobDB";
     private final String portNumber = "1433";
-    private final String userID = "sa";
-    private final String password = "Hngiabao2211";
+    private final String userID = "se150222";
+    private final String password = "jmpemldekukpuual@1";
 
     private DBUtils() {
-        String url = "jdbc:jtds:sqlserver://" + serverName + ":" + portNumber + "/" + dbName;
+        String url = "jdbc:sqlserver://fujob.database.windows.net:1433;database=FuJobDB;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
         try {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         } catch (ClassNotFoundException e) {
         }
         try {
             this.connection = DriverManager.getConnection(url, userID, password);
-        } catch (SQLException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -53,4 +49,21 @@ public class DBUtils {
         return instance;
     }
 
+//    private final String serverName = "localhost";
+//    private final String dbName = "FuJobDB_1";
+//    private final String portNumber = "1433";
+//    private final String userID = "sa";
+//    private final String password = "Hngiabao2211";
+//
+//    private DBUtils() {
+//        String url = "jdbc:jtds:sqlserver://" + serverName + ":" + portNumber + "/" + dbName;
+//        try {
+//            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+//        } catch (ClassNotFoundException e) {
+//        }
+//        try {
+//            this.connection = DriverManager.getConnection(url, userID, password);
+//        } catch (SQLException e) {
+//        }
+//    }
 }
