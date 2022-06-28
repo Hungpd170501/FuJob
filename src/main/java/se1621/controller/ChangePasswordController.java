c80343106a8f0907702959ff64601a8f852ac851
Hungpd170501 <80845607+Hungpd170501@users.noreply.github.com>
2022-06-28 11:44:59 +0700

    Update ChangePasswordController.java

--- src/main/java/se1621/controller/ChangePasswordController.java
+++ src/main/java/se1621/controller/ChangePasswordController.java
@@ -74,7 +74,7 @@ public class ChangePasswordController extends HttpServlet {
             }
         } catch (Exception e) {
             request.setAttribute("LOGIN_MESSAGE", "Something wrong!!");
-            log("Error at LoginController: " + e);
+            log("Error at LoginController: " + e.toString());
         } finally {
             request.getRequestDispatcher(url).forward(request, response);
         }
