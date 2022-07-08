5dbbe5ea5e4be73db7852134c7b0e7df4da76614
Hungpd170501 <hungpd170501@gmail.com>
2022-07-08 23:17:19 +0700

    JDBC Connection Pool- Multithread

--- src/main/java/se1621/utils/DBUtils.java
+++ src/main/java/se1621/utils/DBUtils.java
@@ -1,66 +1,37 @@
 package se1621.utils;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
  * @author ACER
  */
 public class DBUtils {
-
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
     private static DBUtils instance;
-    private Connection connection;
-
     private DBUtils() {
-        String url = "jdbc:sqlserver://fujob2.database.windows.net:1433;database=FuJobDB;encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        } catch (ClassNotFoundException ignored) {
-        }
-        try {
-            String userID = "se150222";
-            String password = "jmpemldekukpuual@1";
-            this.connection = DriverManager.getConnection(url, userID, password);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        config.setJdbcUrl("jdbc:sqlserver://fujob2.database.windows.net:1433;database=FuJobDB;encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+        config.setUsername("se150222");
+        config.setPassword("jmpemldekukpuual@1");
+        config.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
     }
 
-    public Connection getConnection() {
-        return connection;
+    public Connection getConnection() throws SQLException {
+        return ds.getConnection();
     }
 
     public static DBUtils getInstance() {
         if (instance == null) {
             instance = new DBUtils();
-        } else {
-            try {
-                if (instance.getConnection().isClosed()) {
-                    instance = new DBUtils();
-                }
-            } catch (SQLException ignored) {
-                instance = new DBUtils();
-            }
         }
         return instance;
     }
-
-//    private final String serverName = "localhost";
-//    private final String dbName = "FuJobDB_1";
-//    private final String portNumber = "1433";
-//    private final String userID = "sa";
-//    private final String password = "sa";
-//
-//    private DBUtils() {
-//        String url = "jdbc:jtds:sqlserver://" + serverName + ":" + portNumber + "/" + dbName;
-//        try {
-//            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-//        } catch (ClassNotFoundException e) {
-//        }
-//        try {
-//            this.connection = DriverManager.getConnection(url, userID, password);
-//        } catch (SQLException e) {
-//        }
-//    }
 }
